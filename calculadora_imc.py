3e3738b358366081024c690c658d7079b24e72d1
Natália Kiefer <nataliakiefer00@gmail.com>
2025-11-21 11:23:06 -0300

    Fix English wording in comments and strings

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -1,8 +1,8 @@
-# -- importando o módulo de quebra de texto -- 
+# -- importing the text wrap module -- 
 import textwrap
 
 
-# -- Bloco de cores --
+# -- color palette --
 RESET = "\033[0m"
 BOLD = "\033[1m"
 CYAN = "\033[96m"
@@ -15,137 +15,137 @@ ORANGE = "\033[38;5;208m"
 WHITE = "\033[97m"
 
 
-# -- Bloco de declaração de variaveis mutáveis --
-abaixo_peso = peso_normal = sobrepeso = obeso_1 = obeso_2 = obeso_3 = 0
-pacientes = []
-maior_peso = 0
-quantidade_de_pessoas = 0
+# -- variable declarations --
+low_weight = normal_weight = overweight = obese_1 = obese_2 = obese_3 = 0
+pacients = []
+highest_weight = 0
+amount_of_people = 0
 
 
-# -- Bloco de saudação inicial -- 
+# -- initial greeting block -- 
 print(BOLD + BLUE + "=" * 80 + RESET)
-print(BOLD + WHITE + " SEJA BEM VINDO(A) A CALCULADORA DE IMC ".center(80) + RESET)
+print(BOLD + WHITE + " WELCOME TO THE BMI CALCULATOR ".center(80) + RESET)
 print(BOLD + BLUE + "=" * 80 + RESET)
-print(BOLD + CYAN + "Caso queira parar o programa basta digitar 0!".center(80) + RESET)
+print(BOLD + CYAN + "If you want to stop the program, just enter 0!".center(80) + RESET)
 print(BOLD + BLUE + "-" * 80 + RESET)
 
 
-# -- Enquanto o médico não digitar 100 na altura ou no peso -- 
+# -- loop until doctor enters 0 for height, name or weight -- 
 while True:
     try:
-        # -- Coletando os dados -- 
-        nome = str(input(BOLD + YELLOW +"Digite o nome do(a) paciente: " + RESET))
-        peso = float(input("Digite o seu peso: "))
-        altura = float(input("Digite a sua altura: "))
+        # -- Collecting the values -- 
+        name = str(input(BOLD + YELLOW +"Enter the patient´s name: " + RESET))
+        weight = float(input("Enter their weight: "))
+        height = float(input("Enter their height: "))
 
-        # -- Se quiser encerrar -- 
-        if peso == 0 or altura == 0 or nome == 0 :
-            print(BOLD + RED + "programa encerrado" + RESET)
+        # -- if the doctor wants to end the program -- 
+        if weight == 0 or height == 0 or name == 0 :
+            print(BOLD + RED + "END PROGRAM" + RESET)
             break
 
-        # -- calculo do imc -- 
-        calculadora_imc = round(peso / (altura**2), 2)
+        # -- calculating the BMI -- 
+        BMI_calculator = round(weight / (height**2), 2)
 
-        # -- armazenando o valor do imc em uma váriavel -- 
-        valor_imc = calculadora_imc
+        # -- saving the BMI value inside a variable -- 
+        BMI_value = BMI_calculator
 
-        # -- cálculo do peso minimo e maximo ideal -- 
-        peso_ideal = round(18.6 * (altura**2), 2)
-        peso_ideal_max = round(24.9 * (altura**2), 2)
+        # -- calculating the minimum and maximum of the ideal weight  -- 
+        weight_ideal = round(18.6 * (height**2), 2)
+        weight_ideal_max = round(24.9 * (height**2), 2)
 
-        # -- inciando a verificação da classificação do imc -- 
-        if valor_imc <= 18.5:
-            classificacao = 'abaixo do peso'
-            mensagem = "Procure um nutricionista e um médico para entender mais da sua saúde!"
-            abaixo_peso += 1
-        elif valor_imc <= 24.9:
-            classificacao = 'peso saúdavel'
-            mensagem = "Parabéns, continue se cuidando!"
-            peso_normal += 1
-        elif valor_imc <= 29.9:
-            classificacao = "sobrepeso"
-            mensagem = "Busque evitar consumir alimentos ruins para a saúde e muito cáloricos"
-            sobrepeso += 1
-        elif valor_imc <= 34.9:
-            classificacao = "Obesidade grau 1"
-            mensagem = "Busque o auxílio de um nutricionista além de realizar a prática de exercícios físicos"
-            obeso_1 += 1
-        elif valor_imc <= 39.9:
-            classificacao = "Obesidade grau 2"
-            mensagem = 'Busque ajuda médica antes que seja mais difícil reverter a obesidade'
-            obeso_2 += 1
+        # -- checking the classification of the BMI value -- 
+        if BMI_value <= 18.5:
+            classification = "Underweight"
+            message = "See a nutritionist and a doctor to learn more about your health!"
+            low_weight += 1
+        elif BMI_value <= 24.9:
+            classification = "Normal weight "
+            message = "Congratulations, Keep taking care of yourself!"
+            normal_weight += 1
+        elif BMI_value <= 29.9:
+            classification = "Overweight"
+            message = "Avoid unhealthy, high-calorie foods!"
+            overweight += 1
+        elif BMI_value <= 34.9:
+            classification = "Obese 1"
+            message = "See a nutritionist and a doctor. Start doing some physical exercise!"
+            obese_1 += 1
+        elif BMI_value <= 39.9:
+            classification = "Obese 2"
+            message = "Seek clinical help before it becomes harder to reverse the obesity!"
+            obese_2 += 1
         else: 
-            classificacao = "Obesidade grau 3"
-            mensagem = 'Busque ajuda médica o quanto antes para que sua saúde seja restabelecida! '
-            obeso_3 += 1
+            classification = "Obese 3"
+            message = "See a doctor before it becomes too late to restore your health!"
+            obese_3 += 1
         
-        # -- Quantas vezes o sistema rodar armazenar quantas vezes rodou -- 
-        quantidade_de_pessoas += 1
+        # -- Collecting the amount of pacients that entered the program -- 
+        amount_of_people += 1
         
-        # -- mostrando a cada vez que o looping rodar os valores do paciente especifico -- 
+        # -- printing each patient's ideal weight values, medical recommendations, BMI value and its classification -- 
         print(BOLD + BLUE + "\n" + "=" * 60 + RESET)
-        print(BOLD + WHITE + f"Resultado de {nome.upper()}".center(60) + RESET)
+        print(BOLD + WHITE + f"Resulf of {name.upper()}".center(60) + RESET)
         print(BOLD + BLUE + "=" * 60 + RESET)
-        print(f"{WHITE}IMC calculado: {BOLD}{valor_imc}{RESET}")
-        print(f"{WHITE}Classificação: {BOLD}{classificacao}{RESET}")
-        print(f"{WHITE}Peso ideal mínimo: {BOLD}{peso_ideal} kg{RESET}")
-        print(f"{WHITE}Peso ideal máximo: {BOLD}{peso_ideal_max} kg{RESET}")
-        recomendacao_formatada = "\n".join(textwrap.wrap(mensagem, width=40))
-        print(f"{BOLD}Recomendação:{RESET} {recomendacao_formatada}")
+        print(f"{WHITE}BMI value: {BOLD}{BMI_value}{RESET}")
+        print(f"{WHITE}Classification: {BOLD}{classification}{RESET}")
+        print(f"{WHITE}Ideal minimum weight: {BOLD}{weight_ideal} kg{RESET}")
+        print(f"{WHITE}Ideal max weight: {BOLD}{weight_ideal_max} kg{RESET}")
+        recomendation = "\n".join(textwrap.wrap(message, width=40))
+        print(f"{BOLD}Recomendation:{RESET} {recomendation}")
         print(BOLD + BLUE + "-" * 60 + RESET)
 
-        # -- armazenando os valores dos pacientes em um dicionário -- 
-        pacientes.append({
-            'nome' : nome,
-            'peso' : peso,
-            'altura': altura,
-            'IMC': valor_imc,
+        # -- Collecting the values of each pacient in a dictionary -- 
+        pacients.append({
+            'name' : name,
+            'weight' : weight,
+            'height': height,
+            'BMI': BMI_value,
         })        
     
-    # -- Se o usuário realizar uma entrada indevida como texto -- 
+    # -- if the doctor enters a value that isn't the correct number type -- 
     except ValueError:
-        print("Entrada inválida! Digite apenas números.")
+        print("Invallid Entered! Enter only numbers")
     except KeyboardInterrupt:
-        print("Encerrando o programa")
+        print("Ending the program")
         break
 
-# -- Melhorando a visualização das estatísticas --    
-# -- percorrendo os pacientes (se tiver) e coletando dados --
-if pacientes:
-    peso = [i['peso'] for i in pacientes]
-    altura = [i['altura'] for i in pacientes]
-    imc = [i['IMC'] for i in pacientes]
 
-    maior_peso = max(peso)
-    menor_peso = min(peso)
-    maior_altura = max(altura)
-    menor_altura = min(altura)
-    maior_imc = max(imc)
-    menor_imc = min(imc)
+# -- Collecting data of each pacient --
+if pacients:
+    weight = [i['weight'] for i in pacients]
+    height = [i['height'] for i in pacients]
+    BMI = [i['BMI'] for i in pacients]
 
-    # -- calculando a media de imc pela quantidade de pessoas -- 
-    media_imc = round(sum(imc) / quantidade_de_pessoas,2)
+    heighst_weight = max(weight)
+    Lower_weight = min(weight)
+    Higher_height = max(height)
+    Lower_height = min(height)
+    Higher_BMI = max(BMI)
+    Lower_BMI = min(BMI)
+
+    # -- calculating the media of BMI for the amount of pacients -- 
+    BMI_media = round(sum(BMI) / amount_of_people,2)
         
 
-    # -- Bloco para mostrar os resultados -- 
+    # -- Block to show the pacients' results -- 
     print("\n" + BOLD + CYAN + "=" * 80 + RESET)
-    print(BOLD + WHITE + "RESUMO GERAL DOS PACIENTES".center(80) + RESET)
+    print(BOLD + WHITE + "Resume of the pacients".center(80) + RESET)
     print(BOLD + CYAN + "=" * 80 + RESET)
-    print(f"{YELLOW}Abaixo do peso:{RESET} {abaixo_peso}")
-    print(f"{GREEN}Peso normal:{RESET} {peso_normal}")
-    print(f"{ORANGE}Sobrepeso:{RESET} {sobrepeso}")
-    print(f"{RED}Obesidade grau 1:{RESET} {obeso_1}")
-    print(f"{RED}Obesidade grau 2:{RESET} {obeso_2}")
-    print(f"{MAGENTA}Obesidade grau 3:{RESET} {obeso_3}\n")
+    print(f"{YELLOW}Under the weight:{RESET} {low_weight}")
+    print(f"{GREEN}Normal weight:{RESET} {normal_weight}")
+    print(f"{ORANGE}Overweight:{RESET} {overweight}")
+    print(f"{RED}Obesity 1:{RESET} {obese_1}")
+    print(f"{RED}Obesity 2:{RESET} {obese_2}")
+    print(f"{MAGENTA}Obesity 3:{RESET} {obese_3}\n")
 
-    print(f"{WHITE}Maior peso: {BOLD}{maior_peso} kg{RESET}")
-    print(f"{WHITE}Menor peso: {BOLD}{menor_peso} kg{RESET}")
-    print(f"{WHITE}Maior altura: {BOLD}{maior_altura} m{RESET}")
-    print(f"{WHITE}Menor altura: {BOLD}{menor_altura} m{RESET}")
-    print(f"{WHITE}Maior IMC: {BOLD}{maior_imc}{RESET}")
-    print(f"{WHITE}Menor IMC: {BOLD}{menor_imc}{RESET}")
-    print(f"{WHITE}Média geral de IMC: {BOLD}{CYAN}{media_imc}{RESET}")
+    print(f"{WHITE}Higher weight: {BOLD}{heighst_weight} kg{RESET}")
+    print(f"{WHITE}Lower weight: {BOLD}{Lower_weight} kg{RESET}")
+    print(f"{WHITE}Higher height: {BOLD}{Higher_height} m{RESET}")
+    print(f"{WHITE}Lower height: {BOLD}{Lower_height} m{RESET}")
+    print(f"{WHITE}Higher BMI: {BOLD}{Higher_BMI}{RESET}")
+    print(f"{WHITE}Lower BMI: {BOLD}{Lower_BMI}{RESET}")
+    print(f"{WHITE}Midium of BMI: {BOLD}{CYAN}{BMI_media}{RESET}")
 
     print(BOLD + BLUE + "\n" + "=" * 80 + RESET)
-    print(BOLD + WHITE + " FIM DO RELATÓRIO ".center(80) + RESET)
+    print(BOLD + WHITE + " END OF RESUME".center(80) + RESET)
     print(BOLD + BLUE + "=" * 80 + RESET)
